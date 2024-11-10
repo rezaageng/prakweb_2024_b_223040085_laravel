5f319575ddc0512eeb542f77e5bcf3ef4b7e2ebb
rezaa <rezaageng5@gmail.com>
2024-11-10 16:58:19 +0700

    feat(video 13): add video 13

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -8,7 +8,7 @@
     @vite('resources/css/app.css')
     <link rel="stylesheet" href="https://rsms.me/inter/inter.css">
     <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
-    <title>Home</title>
+    <title>{{ $title }}</title>
 </head>
 
 <body class="h-full">
@@ -18,7 +18,6 @@
         <main>
             <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
                 {{ $slot }}
-
             </div>
         </main>
     </div>
